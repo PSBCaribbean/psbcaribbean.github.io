8eedce30885b8995e8a1ad817c56845a1b4823c5
Renee Thomas <reenethom@gmail.com>
2016-12-11 21:45:49 -0600

    Registration page edits completed

--- js/main.js
+++ js/main.js
@@ -1,7 +1,12 @@
 $(document).ready(function(){
-    $(".button-collapse").sideNav();
-});
+    
 
-  $(document).ready(function() {
+    $(".button-collapse").sideNav();
+
     $('select').material_select();
-  });
+
+    $('.datepicker').pickadate({
+       selectMonths: true, // Creates a dropdown to control month
+       selectYears: 100 // Creates a dropdown of 15 years to control year
+     });
+});

a359e1969a22e8a6744860cc211f3ea9ec5854cd
Raman Walwyn-Venugopal <raman.walwyn@gmail.com>
2017-05-05 20:55:10 -0500

    added ability to autoselect combine on registration page based

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
 
     $(".button-collapse").sideNav();
 
-    $('select').material_select();
 
     $('.modal').modal();
 
@@ -11,4 +10,29 @@ $(document).ready(function(){
        selectMonths: true, // Creates a dropdown to control month
        selectYears: 100 // Creates a dropdown of 15 years to control year
      });
+
+    // This code runs on the registration form to 
+    // autoselect the combine when queryparams are passed through from clicking 'register' on an upcoming combine section
+    function getUrlVars(){
+        var vars = [], hash;
+        var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+        for(var i = 0; i < hashes.length; i++)
+        {
+            hash = hashes[i].split('=');
+            vars.push(hash[0]);
+            vars[hash[0]] = hash[1];
+        }
+        return vars;
+    }
+
+    var urlVars = getUrlVars();
+
+    if (urlVars.combine != undefined){
+    	$('#combine-select').val(urlVars.combine)
+    }
+
+    $('select').material_select();
+
+
+
 });
